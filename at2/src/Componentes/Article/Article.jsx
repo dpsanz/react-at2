d05e93e8b8cbd5dc6f4ef9ce6fb8543d91abb70a
Daniel Sanchez <hd2242kin20@gmail.com>
2024-08-20 10:53:12 -0300

    4

--- at2/src/Componentes/Article/Article.jsx
+++ at2/src/Componentes/Article/Article.jsx
@@ -1,38 +1,41 @@
-const Article = ({ title, text, tags, image }) => {
-    return ( 
-    <div 
-    className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl sm:dark:shadow-gray-900 sm:dark:hover:shadow-gray-800 sm:hover:shadow-2xl flex flex-col items-center gap-2">
-      { image ? <div className="relative max-w-xl mx-auto mt-5">
-        <img className="h-64 w-full object-cover rounded-md" src={image} alt={title}/>
-        <div className="absolute inset-0 bg-black opacity-60 rounded-md"></div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h2 className="text-white text-center text-xl font-bold">{title}</h2>
-        </div>
-      </div>
-      :
-      <h2 className="text-black text-center text-xl font-bold">{title}</h2>}
-      
-      <div className="w-full pr-5 flex gap-2 justify-center mt-3">
-        {
-          tags.map(tag => 
-          <span 
-          key={tag} 
-          className="px-4 py-1 cursor-default bg-brand-100 sm:hover:scale-110 hover:bg-brand-200 dark:bg-dark-100 rounded-full text-gray-200 text-xs font-bold uppercase">
-          {tag}
-          </span>)
-        }
-      </div>
-      <div className="flex flex-col gap-1">
-        {
-          text.map((content, index) => 
-          <span 
-          key={index} 
-          className="dark:text-gray-400">
-          {content}
-          </span>)
-        }
-      </div>
-    </div>
-  )}
+// const Article = ({ title, text, tags, image }) => {
+//   return ( 
+//   <div 
+//   className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl sm:dark:shadow-gray-900 sm:dark:hover:shadow-gray-800 sm:hover:shadow-2xl flex flex-col items-center gap-2">
+//     { image ? <div className="relative max-w-xl mx-auto mt-5">
+//       <img className="h-64 w-full object-cover rounded-md" src={image} alt={title}/>
+//       <div className="absolute inset-0 bg-black opacity-60 rounded-md"></div>
+//       <div className="absolute inset-0 flex items-center justify-center">
+//         <h2 className="text-white text-center text-xl font-bold">{title}</h2>
+//       </div>
+//     </div>
+//     :
+//     <h2 className="text-black text-center text-xl font-bold">{title}</h2>}
+    
+//     <div className="w-full pr-5 flex gap-2 justify-center mt-3">
+//       {
+//         tags.map(tag => 
+//         <span 
+//         key={tag} 
+//         className="px-4 py-1 cursor-default bg-brand-100 sm:hover:scale-110 hover:bg-brand-200 dark:bg-dark-100 rounded-full text-gray-200 text-xs font-bold uppercase">
+//         {tag}
+//         </span>)
+//       }
+//     </div>
+//     <div className="flex flex-col gap-1">
+//       {
+//         text.map((content, index) => 
+//         <span 
+//         key={index} 
+//         className="dark:text-gray-400">
+//         {content}
+//         </span>)
+        
+//       }
+//     </div>
+//   </div>
+
   
-  export default Article
+// )}
+
+// export default Article
--- at2/src/Componentes/ArticleList/ArticleList.jsx
+++ at2/src/Componentes/ArticleList/ArticleList.jsx
@@ -1,46 +1,70 @@
-import { useState } from 'react'
+import { useState } from "react";
+import {useMovieSearch} from '../stores/movieService.js'
+import { Link } from "react-router-dom";
 
-import data from '../../../articles.json'
-import Article from '../Article/Article'
-import Input from '../Input/Input'
+const MovieList = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const movies = useMovieSearch(searchInput);
 
-const ArticleList = () => {
-  const[searchInput, setSearchInput] = useState('')
-  const [filteredArticles, setFilteredArticles ] = useState(data)
-  
-  const handleInputChange = e => {
-  
-    const searchTerm = e.target.value
-    setSearchInput(searchTerm)
-
-    const filteredResults = data.filter( (search) => (
-     search.title.toLowerCase().includes(searchTerm.toLowerCase())
-    ))
-
-    setFilteredArticles(filteredResults)
-  }
-
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
 
   return (
     <>
-    <div className='text-center mt-5'>
-      <Input 
-        type="text" 
-        name="searchPost" 
-        id="searchPost"
-        value={searchInput}
-        placeholder="Digite para buscar"
-        onChange={handleInputChange}
+      <div className="text-center mt-5">
+        <input
+          type="text"
+          name="searchMovie"
+          id="searchMovie"
+          value={searchInput}
+          placeholder="Digite para buscar filmes"
+          onChange={handleInputChange}
+          className="p-2 border border-gray-300 rounded"
         />
-    </div>
+      </div>
 
-  <div className="pb-20 sm:mt-5 grid gap-5 sm:grid-cols-2 lg:grid-cols-3 max-w-[1600px]">
-    { filteredArticles.length > 0 ?
-      filteredArticles.map((article, index) => <Article key={index} {...article} />):
-      <p className='text-xl col-span-3 mt-4 text-center dark:text-white'>Nenhum Artigo Encontrado 😔 </p>
-    }
-  </div>
-  </>
-)}
+      <div className="pb-20 sm:mt-5 grid gap-5 sm:grid-cols-2 lg:grid-cols-3 max-w-[1600px]">
+        {movies.length > 0 ? (
+          movies.map((movie, index) => (
+            <div
+              key={index}
+              className="relative w-full h-80 md:h-96 bg-cover bg-center overflow-hidden mb-10"
+              style={{
+                backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${movie.backdrop_path})`,
+              }}
+            >
+              <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center hover:bg-opacity-70 hover:tracking-widest transition-all">
+                <Link
+                  to={`filmes/${movie.id}`}
+                  className="text-rose-800 mt-1 hover:tracking-widest transition-all font-bold"
+                >
+                  <div className="text-white text-center max-w-2xl p-4">
+                    <p className="mt-2 text-xl text-rose-500 font-bold">
+                      AGORA NOS CINEMAS
+                    </p>
+                    <h1 className="text-2xl md:text-5xl font-bold">
+                      {movie.title}
+                    </h1>
+                    <p className="mt-2 text-2xl">
+                      Lançamento: {movie.release_date}{" "}
+                    </p>
+                    <p className="mt-2">
+                      Nota Média de Avaliações: {movie.vote_average}{" "}
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-xl col-span-3 mt-4 text-center dark:text-white">
+            Nenhum Filme Encontrado 😔{" "}
+          </p>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default ArticleList
+export default MovieList;
